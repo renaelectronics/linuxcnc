7f293d3314bcd9dcc495458a77359f5bf0af562b
thomastai <thomastai@localhost.localdomain>
2016-10-09 18:33:07 -0400

    copy files for running stepconf

---
+++ src/emc/usr_intf/stepconf/copy_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cp *.glade /usr/share/linuxcnc/stepconf
+sudo cp pages.py /usr/lib/pymodules/python2.7/pncconf/pages.py
+sudo cp pages.py /usr/lib/pymodules/python2.7/stepconf/pages.py
+sudo cp pages.py /usr/share/pyshared/pncconf/pages.py
+sudo cp pages.py /usr/share/pyshared/stepconf/pages.py
+
